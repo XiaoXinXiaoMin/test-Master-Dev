88a6f70214cf579f8f675222714c6650faf0daf7
20200722-114207\xiaoxin <leiyixin@ikudot.com>
2020-08-17 14:00:33 +0800

    第一次提交测试

---
+++ demo/src/main/java/com/example/demo/service/TestService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+/**
+ * @author xiaoxin
+ */
+public interface TestService {
+}
---
+++ demo/src/main/java/com/example/demo/service/impl/TestServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.demo.service.impl;
+
+/**
+ * @author xiaoxin
+ */
+public class TestServiceImpl {
+}
